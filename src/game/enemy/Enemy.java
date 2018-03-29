f11edf2d6bbad779bb2520872f8eec1ffbb30082
Quan Dinh <dinhhongquan1999@gmail.com>
2018-03-29 14:04:02 +0700

    xD

--- src/game/enemy/Enemy.java
+++ src/game/enemy/Enemy.java
@@ -1,21 +1,21 @@
-package game.enemy;
-
-import base.GameObject;
-import base.Vector2D;
-import utils.Utils;
-
-public class Enemy extends GameObject {
-    public Vector2D velocity;
-
-
-    public Enemy() {
-        this.image = Utils.loadImage("resources/enemyBike/square_deadly_bullet.png");
-        this.velocity = new Vector2D();
-    }
-
-    @Override
-    public void run() {
-        super.run();
-        this.position.addUp(this.velocity);
-    }
-}
+//package game.enemy;
+//
+//import base.GameObject;
+//import base.Vector2D;
+//import utils.Utils;
+//
+//public class Enemy extends GameObject {
+//    public Vector2D velocity;
+//
+//
+//    public Enemy() {
+//        this.image = Utils.loadImage("resources/enemyBike/square_deadly_bullet.png");
+//        this.velocity = new Vector2D();
+//    }
+//
+//    @Override
+//    public void run() {
+//        super.run();
+//        this.position.addUp(this.velocity);
+//    }
+//}
