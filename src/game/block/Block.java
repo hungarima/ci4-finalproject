e81821a2260bacca2da973ec4bd582786f08f68a
Quan Dinh <dinhhongquan1999@gmail.com>
2018-03-29 14:05:34 +0700

    xD

--- src/game/block/Block.java
+++ src/game/block/Block.java
@@ -1,21 +1,21 @@
-package game.block;
-
-import base.GameObject;
-import base.Vector2D;
-import utils.Utils;
-
-public class Block extends GameObject {
-    public Vector2D velocity;
-
-
-    public Block() {
-        this.image = Utils.loadImage("resources/block/block.png");
-        this.velocity = new Vector2D();
-    }
-
-    @Override
-    public void run() {
-        super.run();
-        this.position.addUp(this.velocity);
-    }
-}
+//package game.block;
+//
+//import base.GameObject;
+//import base.Vector2D;
+//import utils.Utils;
+//
+//public class Block extends GameObject {
+//    public Vector2D velocity;
+//
+//
+//    public Block() {
+//        this.image = Utils.loadImage("resources/block/block.png");
+//        this.velocity = new Vector2D();
+//    }
+//
+//    @Override
+//    public void run() {
+//        super.run();
+//        this.position.addUp(this.velocity);
+//    }
+//}
